b664c9afb7d7517cec08c0258f5e791aa3faf38a
alfinsuryaS <alfins132@gmail.com>
2019-09-30 21:02:18 +0800

    Build test

---
+++ build/precache-manifest.912e7840f03a7da0940abb4d9b9afb2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bf6950644cbfe62f847b7e680e46753",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c46641a1515338e13dd0",
+    "url": "/static/js/2.ff7d1350.chunk.js"
+  },
+  {
+    "revision": "ff0c13c2741a0d03ac8a",
+    "url": "/static/js/main.943d1718.chunk.js"
+  },
+  {
+    "revision": "9788183c48219dd179ce",
+    "url": "/static/js/runtime~main.4e4b1550.js"
+  },
+  {
+    "revision": "969daf3d5eb4a7724cb7a1df6eeff2bc",
+    "url": "/static/media/BrokenBack.969daf3d.mp4"
+  },
+  {
+    "revision": "7b2470efd5c7d75b498ade7998479ae9",
+    "url": "/static/media/citrix.7b2470ef.mp4"
+  },
+  {
+    "revision": "6b421abc9e1603131d7052a91c384a96",
+    "url": "/static/media/rainforest.6b421abc.mp4"
+  }
+]);
